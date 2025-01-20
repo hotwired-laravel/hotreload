5923b1d93f8c3fc2ef3157ed58580eb436efbbc5
Tony Messias <tonysm@hey.com>
2025-01-20 14:03:20 -0300

    Wait for server to stop

--- tests/BrowserTestCase.php
+++ tests/BrowserTestCase.php
@@ -2,11 +2,13 @@
 
 namespace Tests;
 
+use Exception;
 use Illuminate\Support\Facades\File;
 use Laravel\Dusk\Browser;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\Dusk\TestCase;
 use Override;
+use PHPUnit\Framework\Attributes\BeforeClass;
 use RecursiveDirectoryIterator;
 
 use function Orchestra\Testbench\package_path;
@@ -37,6 +39,32 @@ class BrowserTestCase extends TestCase
         $this->restoreChangedFiles();
 
         parent::tearDown();
+    }
+
+    #[BeforeClass()]
+    protected function waitForServerToStop()
+    {
+        $i = 0;
+
+        while ($this->isServerUp()) {
+            sleep(1);
+            $i++;
+
+            if ($i >= 5) {
+                throw new Exception('Waited too long for server to stop.');
+            }
+        }
+    }
+
+    protected function isServerUp()
+    {
+        if ($socket = @fsockopen('127.0.0.1', 8001, $errorNumber, $errorString, $timeout = 1)) {
+            fclose($socket);
+
+            return true;
+        }
+
+        return false;
     }
 
     protected function cleanUpFixtures(): void
